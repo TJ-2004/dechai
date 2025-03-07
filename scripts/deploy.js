888458979ee6a253a3eaeff52b7ab96e17e1d4c3
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2025-03-08 00:23:06 +0530

    hardhat and smart constract intialized

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+  const chai = await Chai.deploy(); //creating an instance of our smart contract
+
+  chai.waitForDeployment();//deploying your smart contract
+
+  console.log("Deployed contract address:", await chai.getAddress());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+//0xa64e3144835aF8781c750ceC432784a68d883266
